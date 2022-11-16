55afb98289876c3ba023b820f883e9be30ace665
Vitor Andes <Vitor.andes.santos04@gmail.com>
2022-11-16 19:32:32 -0400

    atualizações de novo código

---
+++ src/projetoIntegrado/TestandoWhile.java
@@ -0,0 +1,13 @@
+package projetoIntegrado;
+
+public class TestandoWhile {
+	public static void main(String[] args) {
+		int contador = 0;
+		
+		
+		while(contador < 10) {
+			contador ++;
+			System.out.print(contador + "," );
+		}
+	}
+}
